33797bfa85a6f183b4dae716f2f822ad3fc8cb19
Nox <aaron.sandgren@live.se>
2019-09-14 17:24:03 +0200

    Button yeets now

--- application/app/src/main/java/com/yahyeet/boardbook/HomeActivity.java
+++ application/app/src/main/java/com/yahyeet/boardbook/HomeActivity.java
@@ -3,12 +3,27 @@ package com.yahyeet.boardbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private String myText = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btn1 = findViewById(R.id.btn1);
+        
+
+        btn1.setOnClickListener((n) -> {btn1.setText("Yeet");});
+
+
+    }
+
+    protected void hello (){
+        myText = "yeet";
     }
 }
