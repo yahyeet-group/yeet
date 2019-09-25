1ad141f3ad20656c9c87d1b975388fe4c4bb5a8f
Carl Holmberg <carl.j.holmberg@telia.com>
2019-09-25 23:42:21 +0200

    minor style fixes to HomeActivity

--- application/app/src/main/java/com/yahyeet/boardbook/activity/HomeActivity.java
+++ application/app/src/main/java/com/yahyeet/boardbook/activity/HomeActivity.java
@@ -17,7 +17,6 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         changeTest();
-
     }
 
     /**
