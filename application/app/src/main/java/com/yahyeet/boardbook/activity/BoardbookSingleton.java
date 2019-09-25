7e537b6f3f6d4ae24dca253c02ac394eb55361dd
Carl Holmberg <carl.j.holmberg@telia.com>
2019-09-25 23:41:17 +0200

    minor style fixes to BoardbookSingleton

--- application/app/src/main/java/com/yahyeet/boardbook/activity/BoardbookSingleton.java
+++ application/app/src/main/java/com/yahyeet/boardbook/activity/BoardbookSingleton.java
@@ -3,22 +3,19 @@ package com.yahyeet.boardbook.activity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.yahyeet.boardbook.model.Boardbook;
-import com.yahyeet.boardbook.model.entity.User;
 import com.yahyeet.boardbook.model.firebase.repository.FirebaseChatGroupRepository;
 import com.yahyeet.boardbook.model.firebase.repository.FirebaseChatMessageRepository;
 import com.yahyeet.boardbook.model.firebase.repository.FirebaseGameRepository;
 import com.yahyeet.boardbook.model.firebase.repository.FirebaseMatchRepository;
 import com.yahyeet.boardbook.model.firebase.repository.FirebaseUserRepository;
 import com.yahyeet.boardbook.model.firebase.service.FirebaseAuthService;
-import com.yahyeet.boardbook.model.handler.AuthHandler;
 import com.yahyeet.boardbook.model.handler.UserHandler;
 import com.yahyeet.boardbook.model.repository.IUserRepository;
 
 public class BoardbookSingleton {
     private static Boardbook instance;
 
-    private BoardbookSingleton() {
-    }
+    private BoardbookSingleton() { }
 
     public static Boardbook getInstance() {
         if (instance == null) {
