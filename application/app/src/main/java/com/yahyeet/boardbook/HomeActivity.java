72758eabbecc4b473ddef9e3318fd4dc6ed747d2
cajoho99 <31243324+cajoho99@users.noreply.github.com>
2019-09-25 10:13:07 +0200

    Update HomeActivity.java

--- application/app/src/main/java/com/yahyeet/boardbook/HomeActivity.java
+++ application/app/src/main/java/com/yahyeet/boardbook/HomeActivity.java
@@ -3,11 +3,6 @@ package com.yahyeet.boardbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.yahyeet.boardbook.model.entity.User;
-import com.yahyeet.boardbook.model.firebase.FirebaseUserRepository;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -16,21 +11,6 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-        FirebaseUserRepository userRepository = new FirebaseUserRepository(firestore);
-
-        User user = new User();
-        user.setEmail("rosen@gren.se");
-        user.setName("Rosen");
-
-        userRepository.create(user).thenAccept(u -> {
-            Log.d("TEST", "User email: " + u.getEmail() + " user id: " + u.getId());
-        }).exceptionally(e -> {
-            Log.d("TEST", e.getMessage());
-            return null;
-        });
-
         setContentView(R.layout.activity_home);
     }
 }
