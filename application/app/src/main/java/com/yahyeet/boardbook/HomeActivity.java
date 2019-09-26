737cb9988eeadc75cc46e953d6ccbad6169bff15
SantaPoro <dinopasalic1999@gmail.com>
2019-09-27 01:21:21 +0200

    navbar working

--- application/app/src/main/java/com/yahyeet/boardbook/HomeActivity.java
+++ application/app/src/main/java/com/yahyeet/boardbook/HomeActivity.java
@@ -1,14 +1,9 @@
 package com.yahyeet.boardbook;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
-
 public class HomeActivity extends AppCompatActivity {
-
     public final String yeet = "Yah-Yeet";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- application/app/src/main/java/com/yahyeet/boardbook/empty_activity.java
+++ application/app/src/main/java/com/yahyeet/boardbook/empty_activity.java
@@ -1,8 +1,13 @@
 package com.yahyeet.boardbook;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.annotation.NonNull;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.view.MenuItem;
+
+        import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class empty_activity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class empty_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty_activity);
+
+        BottomNavigationView bottomnav = findViewById(R.id.bottom_navigation);
+        bottomnav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch(menuItem.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_game:
+                    selectedFragment = new GamesFragment();
+                    break;
+                case R.id.nav_friends:
+                    selectedFragment = new FriendsFragment();
+                    break;
+                case R.id.nav_chat:
+                    selectedFragment = new ChatFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
 }
