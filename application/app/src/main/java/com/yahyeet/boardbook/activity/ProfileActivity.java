577544b1b708683c96879032aa4514558e24acfe
hall <carl.j.holmberg@telia.com>
2019-10-02 11:48:02 +0200

    base profile activity added

---
+++ application/app/src/main/java/com/yahyeet/boardbook/activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.yahyeet.boardbook.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yahyeet.boardbook.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
