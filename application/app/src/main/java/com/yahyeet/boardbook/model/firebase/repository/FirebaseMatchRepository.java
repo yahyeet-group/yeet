7711fef8ff36b50d85dbebf4ec206fda0c03295f
Megavex <42978006+Megavex@users.noreply.github.com>
2019-10-07 01:47:59 +0200

    Revert "Removed unessesary repository interfaces"
    This reverts commit 9914b24b356e139bbd162fa5e454d082f8d6c620.

--- application/app/src/main/java/com/yahyeet/boardbook/model/firebase/repository/FirebaseMatchRepository.java
+++ application/app/src/main/java/com/yahyeet/boardbook/model/firebase/repository/FirebaseMatchRepository.java
@@ -8,7 +8,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.yahyeet.boardbook.model.entity.Match;
-import com.yahyeet.boardbook.model.repository.IRepository;
+import com.yahyeet.boardbook.model.repository.IMatchRepository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 
-public class FirebaseMatchRepository implements IRepository<Match> {
+public class FirebaseMatchRepository implements IMatchRepository {
     private FirebaseFirestore firestore;
 
     public static final String COLLECTION_NAME = "matches";
---
+++ application/app/src/main/java/com/yahyeet/boardbook/model/repository/IMatchRepository.java
@@ -0,0 +1,6 @@
+package com.yahyeet.boardbook.model.repository;
+
+import com.yahyeet.boardbook.model.entity.Match;
+
+public interface IMatchRepository extends IRepository<Match> {
+}
