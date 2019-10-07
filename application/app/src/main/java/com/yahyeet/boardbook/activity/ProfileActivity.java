2e2e50c5399c0aead57fe94b71830158f2cb4e94
Carl Holmberg <carl.j.holmberg@telia.com>
2019-10-07 21:37:53 +0200

    Added functionallity to profile activity

--- application/app/src/main/java/com/yahyeet/boardbook/activity/ProfileActivity.java
+++ application/app/src/main/java/com/yahyeet/boardbook/activity/ProfileActivity.java
@@ -1,16 +1,41 @@
 package com.yahyeet.boardbook.activity;
 
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.yahyeet.boardbook.R;
+import com.yahyeet.boardbook.presenter.ProfilePresenter;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements IProfileActivity {
+
+    private ProfilePresenter profilePresenter;
+    private TextView tvUsername;
+    private TextView tvWinrate;
+    private TextView tvGamesPlayed;
+    private ProgressBar pbWinrate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        profilePresenter = new ProfilePresenter(this);
+
+        tvUsername = findViewById(R.id.tvUsername);
+        tvWinrate = findViewById(R.id.tvWinrate);
+        tvGamesPlayed = findViewById(R.id.tvGamesPlayed);
+        pbWinrate = findViewById(R.id.pbWinrate);
+
+        setDataFields();
+    }
+
+    private void setDataFields() {
+        tvUsername.setText(profilePresenter.getLoggedInUserName());
+        tvWinrate.setText(profilePresenter.getWinratePercentageForLoggedInUser());
+        tvGamesPlayed.setText(profilePresenter.getGamesPlayedForLoggedInUser());
+        pbWinrate.setProgress((int) Math.round(profilePresenter.getWinrateQuotaForLoggedInUser() * 100));
     }
 }
