cfcc8c03c33dbb0483c12caaa587fb8234a0de71
Rasmus Rosengren <rasmus.rosengren@pm.me>
2019-10-07 02:59:33 +0200

    Removed timestamps from abstract entity class

--- application/app/src/main/java/com/yahyeet/boardbook/model/entity/AbstractEntity.java
+++ application/app/src/main/java/com/yahyeet/boardbook/model/entity/AbstractEntity.java
@@ -1,20 +1,10 @@
 package com.yahyeet.boardbook.model.entity;
 
-import java.time.LocalDateTime;
-
 public abstract class AbstractEntity {
     private String id;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-
-    public AbstractEntity(String id, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
 
     public AbstractEntity(String id) {
-        this(id, null, null);
+        this.id = id;
     }
 
     public AbstractEntity() {
@@ -26,21 +16,5 @@ public abstract class AbstractEntity {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
     }
 }
