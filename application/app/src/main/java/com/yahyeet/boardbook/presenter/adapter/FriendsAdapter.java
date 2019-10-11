bc77c75b62150e3b4907c25d7c02d00213fe1874
Santaporo <dinopasalic1999@gmail.com>
2019-10-11 20:12:01 +0200

    added addFriendActivity with visual and fucntionality

--- application/app/src/main/java/com/yahyeet/boardbook/presenter/adapter/FriendsAdapter.java
+++ application/app/src/main/java/com/yahyeet/boardbook/presenter/adapter/FriendsAdapter.java
@@ -19,61 +19,61 @@ import java.util.List;
 
 public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FriendViewHolder> {
 
-    private static final String TAG = "FriendsAdapter";
-    private List<User> myDataset;
+	private static final String TAG = "FriendsAdapter";
+	private List<User> myDataset;
 
-    static class FriendViewHolder extends RecyclerView.ViewHolder {
+	static class FriendViewHolder extends RecyclerView.ViewHolder {
 
-        // TODO Replace this area with match class as a custom view object
-        private TextView friendName;
-        private ImageView friendPicture;
-
-
-        FriendViewHolder(View v) {
-            super(v);
-            // Define click listener for the ViewHolder's View.
-            //v.setOnClickListener(v1 -> Log.d(TAG, "Element " + getAdapterPosition() + " clicked."));
+		// TODO Replace this area with match class as a custom view object
+		private TextView friendName;
+		private ImageView friendPicture;
 
 
-            friendName = v.findViewById(R.id.friendName);
-            friendPicture = v.findViewById(R.id.friendPicture);
-        }
+		FriendViewHolder(View v) {
+			super(v);
+			// Define click listener for the ViewHolder's View.
+			//v.setOnClickListener(v1 -> Log.d(TAG, "Element " + getAdapterPosition() + " clicked."));
 
 
-    }
+			friendName = v.findViewById(R.id.friendName);
+			friendPicture = v.findViewById(R.id.friendPicture);
+		}
 
 
-    public FriendsAdapter(List<User> dataset) {
-        if (dataset != null)
-            myDataset = dataset;
-        else{
-            myDataset = new ArrayList<>();
-        }
-    }
-
-    // Creates new view, does not assign data
-    @NonNull
-    @Override
-    public FriendViewHolder onCreateViewHolder(ViewGroup parent,
-                                              int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.friends_element, parent, false);
-
-        return new FriendViewHolder(v);
-    }
-
-    // Replace the contents of a view (invoked by the layout manager)
-    // Method that assigns data to the view
-    @Override
-    public void onBindViewHolder(FriendViewHolder holder, int position) {
+	}
 
 
-        holder.friendName.setText(myDataset.get(position).getName());
-        //holder.friendPicture.setImageURI();
-    }
+	public FriendsAdapter(List<User> dataset) {
+		if (dataset != null)
+			myDataset = dataset;
+		else {
+			myDataset = new ArrayList<>();
+		}
+	}
 
-    @Override
-    public int getItemCount() {
-        return myDataset.size();
-    }
+	// Creates new view, does not assign data
+	@NonNull
+	@Override
+	public FriendViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		View v = LayoutInflater
+			.from(parent.getContext())
+			.inflate(R.layout.friends_element, parent, false);
+
+		return new FriendViewHolder(v);
+	}
+
+	// Replace the contents of a view (invoked by the layout manager)
+	// Method that assigns data to the view
+	@Override
+	public void onBindViewHolder(FriendViewHolder holder, int position) {
+
+
+		holder.friendName.setText(myDataset.get(position).getName());
+		//holder.friendPicture.setImageURI();
+	}
+
+	@Override
+	public int getItemCount() {
+		return myDataset.size();
+	}
 }
