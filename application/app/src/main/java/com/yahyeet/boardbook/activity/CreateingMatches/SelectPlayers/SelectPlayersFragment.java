96e55379b2c146eb7f3e657c1b9ff9ae600c9b0d
Aaron Sandgren <aaron.sandgren@live.se>
2019-10-14 22:32:01 +0200

    Initial design comeplete, some bugfixing still left do to but groundwork done

--- application/app/src/main/java/com/yahyeet/boardbook/activity/CreateingMatches/SelectPlayers/SelectPlayersFragment.java
+++ application/app/src/main/java/com/yahyeet/boardbook/activity/CreateingMatches/SelectPlayers/SelectPlayersFragment.java
@@ -6,8 +6,10 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,45 +19,70 @@ import androidx.fragment.app.Fragment;
 import com.yahyeet.boardbook.R;
 import com.yahyeet.boardbook.presenter.SelectPlayerPresenter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelectPlayersFragment extends Fragment implements ISelectPlayersFragment {
 
-    private SelectPlayerPresenter spp;
+	private SelectPlayerPresenter spp;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        spp = new SelectPlayerPresenter(this);
-        return inflater.inflate(R.layout.fragment_select_players, container, false);
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		spp = new SelectPlayerPresenter(this);
+		return inflater.inflate(R.layout.fragment_select_players, container, false);
 
-    }
+	}
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        bindTestButtons();
-    }
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		bindTestButtons();
+	}
 
-    private void bindTestButtons() {
-        View v = getView();
-        View testView = v.findViewById(R.id.testingLayout);
+	private void bindTestButtons() {
+		View v = getView();
+		View testView = v.findViewById(R.id.testingLayout);
 
-        Button cssBtn = v.findViewById(R.id.cssTestBtn);
-        Button csbBtn = v.findViewById(R.id.csbTestingBtn);
-        Button ccBtn = v.findViewById(R.id.ccTestingBtn);
+		Button cssBtn = v.findViewById(R.id.spTestButton);
+		Button editButton = v.findViewById(R.id.spEditButton);
+		Button doneButton = v.findViewById(R.id.spDoneButton);
 
-        cssBtn.setOnClickListener((n) -> {
-            //testView.getLayoutParams().height = 0;
-          //  testView.requestLayout();
-            ObjectAnimator animation = ObjectAnimator.ofFloat(testView, "transformationX", 100f);
-            animation.setDuration(2000);
-            animation.start();
-        });
-        csbBtn.setOnClickListener((n) -> {
-            testView.getLayoutParams().height = 400;
-            testView.requestLayout();
-        });
-        ccBtn.setOnClickListener((n) -> {
-            testView.setBackgroundColor(101010);
-        });
-    }
+
+		//// SPINNER INIT
+		List<String> spinnerArray =  new ArrayList<String>();
+		spinnerArray.add("Minions Of Mordred");
+		Spinner teamSpinner = v.findViewById(R.id.teamSpinner);
+		ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, spinnerArray);		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		teamSpinner.setAdapter(adapter);
+
+		cssBtn.setOnClickListener((n) -> {
+			testView.getLayoutParams().height = dpFromPx(100);
+			testView.requestLayout();
+		});
+		editButton.setOnClickListener((n) -> {
+			testView.getLayoutParams().height = dpFromPx(250);
+			testView.requestLayout();
+		});
+		doneButton.setOnClickListener((n) -> {
+			System.out.println("YEEEEEEEEEEEEEET!");
+			testView.getLayoutParams().height = dpFromPx(100);
+			testView.getLayoutParams();
+		});
+		/*
+			csbBtn.setOnClickListener((n) -> {
+			testView.getLayoutParams().height = 400;
+			testView.requestLayout();
+			*/
+	/*});
+		ccBtn.setOnClickListener((n) -> {
+			testView.setBackgroundColor(101010);
+		});*/
+	}
+
+	public int dpFromPx(int px) {
+		return (int) (px * getContext().getResources().getDisplayMetrics().density);
+	}
 }
