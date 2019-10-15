4176e500a49e3cfee181b4ba66b35b19cb6610c3
Carl Holmberg <carl.j.holmberg@telia.com>
2019-10-15 11:51:42 +0200

    Refined authentication tests

--- application/app/src/test/java/com/yahyeet/boardbook/AuthenticationTest.java
+++ application/app/src/test/java/com/yahyeet/boardbook/AuthenticationTest.java
@@ -4,8 +4,12 @@ import com.yahyeet.boardbook.model.entity.User;
 import com.yahyeet.boardbook.model.handler.AuthHandler;
 import com.yahyeet.boardbook.model.handler.UserHandler;
 import com.yahyeet.boardbook.model.mock.service.MockAuthService;
+import com.yahyeet.boardbook.model.mock.service.MockMatchRepository;
+import com.yahyeet.boardbook.model.mock.service.MockUserRepository;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -13,40 +17,45 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class AuthenticationTest {
+	List<MockAuthService.AuthenticationUser> authenticationUsers;
+	MockAuthService mockAuthService;
+	AuthHandler authHandler;
+
+	@Before
+	public void initTest() {
+		authenticationUsers = new ArrayList<>();
+		mockAuthService = new MockAuthService(authenticationUsers);
+		MockUserRepository mockUserRepository = new MockUserRepository();
+		UserHandler userHandler = new UserHandler(mockUserRepository, new MockMatchRepository(mockUserRepository));
+		authHandler = new AuthHandler(mockAuthService, userHandler);
+	}
+
+	@After
+	public void cleanTest() {
+		authenticationUsers = null;
+	}
+
 	@Test
 	public void login() {
-		List<MockAuthService.AuthenticationUser> authenticationUsers = new ArrayList<>();
-		authenticationUsers.add(new MockAuthService.AuthenticationUser("test@test.test", "MyPassword", new User("id", "Testificate")));
-		MockAuthService mockAuthService = new MockAuthService(authenticationUsers);
-		UserHandler userHandler = new UserHandler(new MockUserRepository());
-		AuthHandler authHandler = new AuthHandler(mockAuthService, userHandler);
+		authenticationUsers.add(new MockAuthService.AuthenticationUser("test@test.test", "MyPassword", new User("Testificate")));
 
 		try {
 			mockAuthService.login("test@test.test", "MyPassword").thenAccept(user -> {
 				Assert.assertEquals("Testificate", user.getName());
-				Assert.assertEquals("id", user.getId());
-				Assert.assertEquals("test@test.test", user.getEmail());
 			}).get();
 		} catch (ExecutionException | InterruptedException e) {
 			Assert.fail();
 		}
-
 		Assert.assertEquals("Testificate", authHandler.getLoggedInUser().getName());
 	}
 
 	@Test
 	public void register() {
-		List<MockAuthService.AuthenticationUser> authenticationUsers = new ArrayList<>();
-		MockAuthService mockAuthService = new MockAuthService(authenticationUsers);
-		UserHandler userHandler = new UserHandler(new MockUserRepository());
-		AuthHandler authHandler = new AuthHandler(mockAuthService, userHandler);
-
 		try {
 			mockAuthService.signup("prov@prov.prov", "MinKod", "Testare").thenAccept(user -> {
 				Assert.assertNotEquals(0, authenticationUsers.size());
 				Assert.assertEquals("Testare", authenticationUsers.get(0).user.getName());
 				Assert.assertEquals("MinKod", authenticationUsers.get(0).password);
-				Assert.assertEquals("prov@prov.prov", authenticationUsers.get(0).user.getEmail());
 			}).get();
 		} catch (ExecutionException | InterruptedException e) {
 			Assert.fail();
@@ -56,9 +65,6 @@ public class AuthenticationTest {
 	@Test
 	public void logout() {
 		List<MockAuthService.AuthenticationUser> authenticationUsers = new ArrayList<>();
-		MockAuthService mockAuthService = new MockAuthService(authenticationUsers);
-		UserHandler userHandler = new UserHandler(new MockUserRepository());
-		AuthHandler authHandler = new AuthHandler(mockAuthService, userHandler);
 
 		authHandler.setLoggedInUser(new User());
 		Assert.assertNotEquals(null, authHandler.getLoggedInUser());
