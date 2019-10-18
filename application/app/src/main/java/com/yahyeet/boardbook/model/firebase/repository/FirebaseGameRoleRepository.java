8fbdc774af45759eb1863ec5918a5ff1e27eb811
Rasmus Rosengren <rasmus.rosengren@pm.me>
2019-10-18 02:37:33 +0200

    Find game roles by a game team

--- application/app/src/main/java/com/yahyeet/boardbook/model/firebase/repository/FirebaseGameRoleRepository.java
+++ application/app/src/main/java/com/yahyeet/boardbook/model/firebase/repository/FirebaseGameRoleRepository.java
@@ -1,13 +1,18 @@
 package com.yahyeet.boardbook.model.firebase.repository;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.yahyeet.boardbook.model.entity.GameRole;
 import com.yahyeet.boardbook.model.repository.IGameRoleRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.stream.Collectors;
 
 public class FirebaseGameRoleRepository extends AbstractFirebaseRepository<GameRole> implements IGameRoleRepository {
 	public FirebaseGameRoleRepository(FirebaseFirestore firestore) {
@@ -31,6 +36,33 @@ public class FirebaseGameRoleRepository extends AbstractFirebaseRepository<GameR
 
 	@Override
 	public CompletableFuture<List<GameRole>> findRolesByTeamId(String id) {
-		return CompletableFuture.completedFuture(new ArrayList<>());
+		return CompletableFuture.supplyAsync(() -> {
+			Task<QuerySnapshot> task =
+				getFirestore()
+					.collection(getCollectionName())
+					.whereEqualTo("teamId", id)
+					.get();
+
+			try {
+				QuerySnapshot querySnapshot = Tasks.await(task);
+
+				return querySnapshot
+					.getDocuments()
+					.stream()
+					.map(this::fromDocumentToFirebaseEntity)
+					.collect(Collectors.toList());
+			} catch (Exception e) {
+				throw new CompletionException(e);
+			}
+		}).thenApply(firebaseGameRoles -> {
+			firebaseGameRoles.forEach(firebaseGameRole -> {
+				getCache().put(firebaseGameRole.getId(), firebaseGameRole);
+			});
+
+			return firebaseGameRoles
+				.stream()
+				.map(FirebaseEntity::toModelType)
+				.collect(Collectors.toList());
+		});
 	}
 }
