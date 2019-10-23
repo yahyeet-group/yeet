36612e289308b2723e8f17ea7e89e5ecc3d496c1
Carl Holmberg <carl.j.holmberg@telia.com>
2019-10-23 19:24:27 +0200

    Fixed broken test in User Handler

--- application/app/src/test/java/com/yahyeet/boardbook/model/handlers/UserHandlerTest.java
+++ application/app/src/test/java/com/yahyeet/boardbook/model/handlers/UserHandlerTest.java
@@ -26,142 +26,144 @@ import static org.junit.Assert.fail;
 
 public class UserHandlerTest {
 
-    private UserHandler userHandler;
-    private MatchHandler matchHandler;
-    private GameHandler gameHandler;
+	private UserHandler userHandler;
+	private MatchHandler matchHandler;
+	private GameHandler gameHandler;
 
 
-    private MockUserRepository userRepository;
-    private MockMatchRepository matchRepository;
-    private MockGameRoleRepository roleRepository;
-    private MockGameTeamRepository teamRepository;
-    private MockGameRepository gameRepository;
-    private MockMatchPlayerRepository playerRepository;
+	private MockUserRepository userRepository;
+	private MockMatchRepository matchRepository;
+	private MockGameRoleRepository roleRepository;
+	private MockGameTeamRepository teamRepository;
+	private MockGameRepository gameRepository;
+	private MockMatchPlayerRepository playerRepository;
 
-    @Before
-    public void initTests() {
-        userRepository = new MockUserRepository();
-        matchRepository = new MockMatchRepository();
-        roleRepository = new MockGameRoleRepository();
-        teamRepository = new MockGameTeamRepository();
-        gameRepository = new MockGameRepository();
-        playerRepository = new MockMatchPlayerRepository();
+	@Before
+	public void initTests() {
+		userRepository = new MockUserRepository();
+		matchRepository = new MockMatchRepository();
+		roleRepository = new MockGameRoleRepository();
+		teamRepository = new MockGameTeamRepository();
+		gameRepository = new MockGameRepository();
+		playerRepository = new MockMatchPlayerRepository();
 
-        userHandler = new UserHandler(
-                userRepository,
-                matchRepository,
-                roleRepository,
-                teamRepository,
-                gameRepository,
-                playerRepository
-        );
+		userHandler = new UserHandler(
+			userRepository,
+			matchRepository,
+			roleRepository,
+			teamRepository,
+			gameRepository,
+			playerRepository
+		);
 
-        matchHandler = new MatchHandler(
-                matchRepository,
-                playerRepository,
-                gameRepository,
-                roleRepository,
-                teamRepository,
-                userRepository
-        );
+		matchHandler = new MatchHandler(
+			matchRepository,
+			playerRepository,
+			gameRepository,
+			roleRepository,
+			teamRepository,
+			userRepository
+		);
 
-        gameHandler = new GameHandler(
-                gameRepository,
-                roleRepository,
-                teamRepository,
-                matchRepository
-        );
-    }
+		gameHandler = new GameHandler(
+			gameRepository,
+			roleRepository,
+			teamRepository,
+			matchRepository
+		);
+	}
 
-    @Test
-    public void saveUserAndGet(){
-        User user = new User("Carl");
-        try{
-            String id = userHandler.save(user).get().getId();
-            User result = userHandler.find(id).get();
-            assertEquals(user, result);
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
+	@Test
+	public void saveUserAndGet() {
+		User user = new User("Carl");
+		try {
+			String id = userHandler.save(user).get().getId();
+			User result = userHandler.find(id).get();
+			assertEquals(user, result);
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Test
-    public void getAllTest() {
-        User user1 = new User("Carlos");
-        User user2 = new User("Alexanderos");
-        User user3 = new User("Aaronos");
-        User user4 = new User("Dinos");
-        User user5 = new User("Rasmusos");
+	@Test
+	public void getAllTest() {
+		User user1 = new User("Carlos");
+		User user2 = new User("Alexanderos");
+		User user3 = new User("Aaronos");
+		User user4 = new User("Dinos");
+		User user5 = new User("Rasmusos");
 
-        try {
-            userHandler.save(user1).get();
-            userHandler.save(user2).get();
-            userHandler.save(user3).get();
-            userHandler.save(user4).get();
-            userHandler.save(user5).get();
-            int size = userHandler.all().get().size();
-            assertEquals(5, size);
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
+		try {
+			userHandler.save(user1).get();
+			userHandler.save(user2).get();
+			userHandler.save(user3).get();
+			userHandler.save(user4).get();
+			userHandler.save(user5).get();
+			int size = userHandler.all().get().size();
+			assertEquals(5, size);
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void saveUserWithFriendAndGet(){
-        User user1 = new User("Carl");
-        User user2 = new User("Rasmus");
-        user1.addFriend(user2);
-        try{
-            userHandler.save(user1).get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
+	@Test
+	public void saveUserWithFriendAndGet() {
+		User user1 = new User("Carl");
+		user1.setId("carlId");
+		User user2 = new User("Rasmus");
+		user2.setId("rasmusId");
+		user1.addFriend(user2);
+		try {
+			userHandler.save(user1).get();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Test
-    public void saveUserWithMatchPlayerAndGet() {
-        Game game = new Game(
-                "Avalon",
-                "Cool game where you laugh when someone accuses you of being Oberon",
-                4,
-                5,
-                10
-        );
-        GameTeam gt = new GameTeam("Team name");
-        gt.addRole(new GameRole("Role name"));
+	@Test
+	public void saveUserWithMatchPlayerAndGet() {
+		Game game = new Game(
+			"Avalon",
+			"Cool game where you laugh when someone accuses you of being Oberon",
+			4,
+			5,
+			10
+		);
+		GameTeam gt = new GameTeam("Team name");
+		gt.addRole(new GameRole("Role name"));
 
-        game.addTeam(gt);
-        try {
-            game = gameHandler.save(game).get();
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
+		game.addTeam(gt);
+		try {
+			game = gameHandler.save(game).get();
+		} catch (ExecutionException | InterruptedException e) {
+			e.printStackTrace();
+		}
 
-        Match match = new Match(game);
-        User user = new User();
-        try {
-            user = userHandler.save(user).get();
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
+		Match match = new Match(game);
+		User user = new User();
+		try {
+			user = userHandler.save(user).get();
+		} catch (ExecutionException | InterruptedException e) {
+			e.printStackTrace();
+		}
 
-        GameTeam gameTeam = game.getTeams().get(0);
-        GameRole gameRole = game.getTeams().get(0).getRoles().get(0);
+		GameTeam gameTeam = game.getTeams().get(0);
+		GameRole gameRole = game.getTeams().get(0).getRoles().get(0);
 
 
-        MatchPlayer player = new MatchPlayer(user, gameRole, gameTeam, false);
+		MatchPlayer player = new MatchPlayer(user, gameRole, gameTeam, false);
 
-        try {
+		try {
 
-            match.addMatchPlayer(player);
-            matchHandler.save(match).get();
-            String id = userHandler.save(user).get().getId();
-            User result = userHandler.find(user.getId()).get();
-            assertEquals(user, result);
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
+			match.addMatchPlayer(player);
+			matchHandler.save(match).get();
+			String id = userHandler.save(user).get().getId();
+			User result = userHandler.find(user.getId()).get();
+			assertEquals(user, result);
+		} catch (ExecutionException | InterruptedException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
 }
