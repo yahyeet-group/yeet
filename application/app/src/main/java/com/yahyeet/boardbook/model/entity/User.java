b52117679309923bd519a97774d60910a9aa032f
Carl Holmberg <carl.j.holmberg@telia.com>
2019-10-23 19:10:15 +0200

    User cannot add itself as friend

--- application/app/src/main/java/com/yahyeet/boardbook/model/entity/User.java
+++ application/app/src/main/java/com/yahyeet/boardbook/model/entity/User.java
@@ -1,47 +1,45 @@
 package com.yahyeet.boardbook.model.entity;
 
-import androidx.annotation.Nullable;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class User extends AbstractEntity {
-    private String name;
-    private List<User> friends = new ArrayList<>();
-    private List<Match> matches = new ArrayList<>();
+	private String name;
+	private List<User> friends = new ArrayList<>();
+	private List<Match> matches = new ArrayList<>();
 
-    public User(String name) {
-        this.name = name;
-    }
+	public User(String name) {
+		this.name = name;
+	}
 
-    public User() {
-    }
+	public User() {
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public List<User> getFriends() {
-        return friends;
-    }
+	public List<User> getFriends() {
+		return friends;
+	}
 
-    public void addFriend(User friend) {
-        if(getFriends().stream().anyMatch(f -> f.equals(friend))){
-            return;
-        }
-        friends.add(friend);
-        friend.addFriend(this);
-    }
+	public void addFriend(User friend) {
+		if (friend.equals(this) || getFriends().stream().anyMatch(f -> f.equals(friend))) {
+			return;
+		}
+		friends.add(friend);
+		friend.addFriend(this);
+	}
 
-    public List<Match> getMatches() {
-        return matches;
-    }
+	public List<Match> getMatches() {
+		return matches;
+	}
 
-    public void addMatch(Match match) {
-        matches.add(match);
-    }
+	public void addMatch(Match match) {
+		matches.add(match);
+	}
 }
